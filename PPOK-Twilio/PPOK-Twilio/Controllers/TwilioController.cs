605520fdadd958d6f5e6c4aa2c67620dc7476a61
Emily Pielemeier <emily.pielemeier@eagles.oc.edu>
2017-03-01 18:13:13 -0600

    config for twilio and basic send SMS message function

---
+++ PPOK-Twilio/PPOK-Twilio/Controllers/TwilioController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PPOK_Twilio.Controllers
+{
+    public class TwilioController : Controller
+    {
+        
+        public ActionResult SendMessageExample(string toPhoneNumber, string messageBody)
+        {
+            var message = PPOK.Domain.Service.TwilioService.SendSMSMessage(toPhoneNumber, messageBody);
+            return View();
+        }
+        
+    }
+}
---
+++ PPOK-Twilio/PPOK.Domain/Service/TwilioService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+
+namespace PPOK.Domain.Service
+{
+    public class TwilioService
+    {
+        private static readonly string AccountSid = "ACdc4baa0b5fceb40de713632d9ed04e7d";
+        private static readonly string AuthToken = "2dda1785d4239e9cb0bb8c251f3ff0d9";
+        private static readonly string MessageServiceSid = "MG546d4d7950cc9497c1b5dbdd89fe9829";
+
+        public static MessageResource SendSMSMessage(string toNumber, string messageBody)
+        {
+            TwilioClient.Init(AccountSid, AuthToken);
+
+            var message = MessageResource.Create(
+                 to: new PhoneNumber(toNumber),
+                 messagingServiceSid: MessageServiceSid,
+                 body: messageBody);
+
+            if (message.ErrorCode != null)
+            {
+                throw new Exception("Twilio encountered an error: " + message.ErrorCode + " - " + message.ErrorMessage);
+            }
+            if (message.DateSent == null)
+            {
+                throw new Exception("Twilio did not send the message, see Twilio's console log for details.");
+            }
+
+            return message;
+        }
+    }
+}
+
