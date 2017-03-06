4c2677e2a59cea1bb19bc8b6749c9ad1f63afe90
VALKYRIE\christopher.sartin <christopher.sartin@eagles.oc.edu>
2017-03-06 13:59:53 -0600

    Remebered how to MVC.

---
+++ PPOK-Twilio/PPOK-Twilio/Controllers/PharmacistLandingController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PPOK_Twilio.Controllers
+{
+    public class PharmacistLandingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
