61c614c813312edaa44550a37764687433b70157
VALKYRIE\christopher.sartin <christopher.sartin@eagles.oc.edu>
2017-03-08 21:48:39 -0600

    I've got a button that doesn't work... but on the bright side it doesn't break anything.

---
+++ PPOK-Twilio/PPOK-Twilio/Scripts/csvPicker.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+
+    $("#go").click(function () {
+
+        var xhr = new XMLHttpRequest();
+        var fd = new FormData();
+        fd.append("file", document.getElementById('fileInput').files[0]);
+        xhr.open("POST", "/PharmacistLanding/UploadContact/", true);
+        xhr.send(fd);
+        xhr.addEventListener("load", function (event) {
+            alert(event.target.response);
+        }, false);
+
+    });
+
+});
