c21e179e9ce7b437d2e63c49316077371200a951
VALKYRIE\christopher.sartin <christopher.sartin@eagles.oc.edu>
2017-03-09 00:04:45 -0600

    Got simple page running. Small errors with the model.

---
+++ PPOK-Twilio/PPOK-Twilio/Controllers/PatientMCPController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PPOK_Twilio.Controllers
+{
+    public class PatientMCPController : Controller
+    {
+        // GET: PatientMCP
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
