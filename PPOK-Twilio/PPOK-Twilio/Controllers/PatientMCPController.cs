ccac76e519e3a8d5f8405bf02897df413203f4dd
VALKYRIE\christopher.sartin <christopher.sartin@eagles.oc.edu>
2017-03-09 19:13:50 -0600

    Database is working!

--- PPOK-Twilio/PPOK-Twilio/Controllers/PatientMCPController.cs
+++ PPOK-Twilio/PPOK-Twilio/Controllers/PatientMCPController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PPOK.Domain.Service;
+using PPOK.Domain.Types;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,18 @@ namespace PPOK_Twilio.Controllers
         // GET: PatientMCP
         public ActionResult Index()
         {
-            return View();
+            using (var service = new PatientService())
+            {
+                var patient = service.GetWhere(PatientService.EmailCol == "christopher.sartin@eagles.oc.edu").FirstOrDefault();
+                //Patient patient = new Patient();
+                //patient.FirstName = "Christopher";
+                //patient.LastName = "Sartin";
+                //patient.ContactPreference = ContactPreference.PHONE;
+                //patient.Phone = "918-399-4836";
+                //patient.Email = "christopher.sartin@eagles.oc.edu";
+
+                return View(patient);
+            }
         }
     }
 }
