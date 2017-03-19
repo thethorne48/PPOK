558da8340191909d684bdaf76c6f8a2b9c6f94a0
mattmiller64 <miller.matthew64@gmail.com>
2017-03-19 13:58:28 -0500

    rudimentary changes made to allow datatables, search page created, waiting on error to be looked at with jon

---
+++ PPOK-Twilio/PPOK-Twilio/Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using PPOK.Domain.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PPOK_Twilio.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult GetAllEvents()
+        {
+            using (var service = new EventService())
+            {
+                var test = service.GetAll();
+                
+                return Json(test);
+            }
+        }
+    }
+}
---
+++ PPOK-Twilio/PPOK-Twilio/PageScripts/Search.js
@@ -0,0 +1,76 @@
+﻿window.Search = (function ($) {
+    function alertstuff(id) {
+        //if (confirm("Are you sure you want to delete this record? " + id)) {
+        //    $.ajax({
+        //        type: "POST",
+        //        url: "/ManageBadge/Delete",
+        //        data: { id },
+        //        dataType: "json",
+        //        success: function (r) {
+        //            window.history.go(0);
+        //        }
+        //    });
+        //}
+    };
+    function EditBadge(id) {
+        //console.log("Got here taco : " + id);
+        //$.ajax({
+        //    type: "POST",
+        //    url: "/ManageBadge/GetSingleBadge",
+        //    data: { id },
+        //    dataType: "json",
+        //    success: function (r) {
+        //        console.log(r);
+        //        document.getElementById("editId").value = r.result.ID;
+        //        document.getElementById("editName").value = r.result.Name;
+        //        document.getElementById("editDescript").value = r.result.Descript || "";
+        //        var element = document.getElementById('editSenderType');
+        //        element.value = r.result2;
+        //        var element2 = document.getElementById('editBadgeType');
+        //        element2.value = r.result3;
+        //        document.getElementById("editImgLink").value = r.result.ImgLink;
+        //        document.getElementById("editActDate").value = new Date(parseInt(r.result.ActDate.substr(6))).toLocaleDateString("en-US") || "";
+        //        console.log(new Date(parseInt(r.result.ActDate.substr(6))).toLocaleDateString("en-US"));
+        //        if (r.result.DeactDate != null)
+        //            document.getElementById("editDeactDate").value = new Date(parseInt(r.result.DeactDate.substr(6))).toLocaleDateString("en-US");
+        //        else
+        //            document.getElementById("editDeactDate").value = "";
+        //        $('#EditModal').modal('toggle');
+        //    }
+        //});
+    };
+    return {
+        init: function () {
+            console.log("Loading JS");
+            var dataTaco;
+            $.ajax({
+                type: "POST",
+                url: "/Search/GetAllEvents",
+                dataType: "json",
+                success: function (r) { //recursion limit exceeded
+                    console.log(r);
+                    dataTaco = r;
+                    //$('#example').DataTable({
+                    //    data: dataTaco,
+                    //    columns: [
+                    //        { title: "Name" },
+                    //        { title: "Position" },
+                    //        { title: "Office" },
+                    //        { title: "Extn." },
+                    //        { title: "Start date" },
+                    //        { title: "Salary" }
+                    //    ]
+                    //});
+                }
+            });
+            console.log("finished loading js");
+        },
+        alertstuff: function (id) {
+            alertstuff(id);
+        },
+        EditBadge: function (item) {
+            EditBadge(item);
+        },
+    }
+
+})(jQuery);
