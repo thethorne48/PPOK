9fc51e9664d49245be9410aff96019604f344340
Emily Pielemeier <emily.pielemeier@eagles.oc.edu>
2017-03-23 14:09:10 -0500

    simple file upload using ajax

---
+++ PPOK-Twilio/PPOK-Twilio/Controllers/RecallController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PPOK_Twilio.Controllers
+{
+    public class RecallController : Controller
+    {
+        // GET: Recall
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload()
+        {
+            string path = "";
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = "Recall.csv";// Path.GetFileName(file.FileName);
+                    path = Path.Combine(Server.MapPath("~/App_Data/Temp"), fileName);
+                    file.SaveAs(path);
+                }
+            }
+
+            return Json(new { fileName = path });
+        }
+    }
+}
