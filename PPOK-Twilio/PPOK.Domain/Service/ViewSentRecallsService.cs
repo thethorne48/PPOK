bf6a41c60aafe09246c05d3b89e48bcf80120e69
tomjhartnett <Tom.Hartnett@eagles.oc.edu>
2017-03-29 11:08:06 -0500

    viewSentRecallsService added + minor fixes

---
+++ PPOK-Twilio/PPOK.Domain/Service/ViewSentRecallsService.cs
@@ -0,0 +1,24 @@
+﻿using PPOK.Domain.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPOK.Domain.Service
+{
+    class ViewSentRecallsService
+    {
+        public List<Event> getSentEvents(EventStatus status)
+        {
+            List<Event> list = new List<Event>();
+            using (var service = new EventHistoryService())
+            {
+                var events = service.GetWhere(EventHistoryService.StatusCol == status);
+                foreach (EventHistory h in events)
+                    list.Add(h.Event);
+            }
+            return list;
+        }
+    }
+}
---
+++ PPOK-Twilio/PPOK.Domain/Tests/SentEventsTest.cs
@@ -0,0 +1,33 @@
+﻿using PPOK.Domain.Service;
+using PPOK.Domain.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPOK.Domain.Tests
+{
+    class SentEventsTest
+    {
+        //public static void Main()
+        //{
+        //    try
+        //    {
+        //        ViewSentRecallsService init = new ViewSentRecallsService();
+        //        Console.WriteLine("Generating Events...");
+        //        EventStatus status = EventStatus.test;
+        //        var events =  init.getSentEvents(status);
+        //        Console.WriteLine("Events grabbed successfully.\nAll tests successful...\n");
+
+        //        foreach (Event e in events)
+        //            Console.WriteLine(e.ToString());
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex);
+        //        Console.ReadKey();
+        //    }
+        //}
+    }
+}
