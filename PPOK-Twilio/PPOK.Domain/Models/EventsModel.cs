11111de2afdff566d197a9b385852c991cdf9903
tomjhartnett <Tom.Hartnett@eagles.oc.edu>
2017-04-10 20:34:07 -0500

    basic framework

---
+++ PPOK-Twilio/PPOK.Domain/Models/EventsModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PPOK.Domain.Service;
+using PPOK.Domain.Types;
+
+namespace PPOK.Domain.Models
+{
+    public class EventsModel
+    {
+        public List<Prescription> Prescriptions { get; set; }
+
+        public EventsModel()
+        {
+
+        }
+
+        public EventsModel(List<Prescription> list)
+        {
+            Prescriptions = list;
+        }
+    }
+}
