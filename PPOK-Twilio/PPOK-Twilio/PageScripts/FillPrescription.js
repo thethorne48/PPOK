8010acc03a62be5978f8a0818e8ac648eceecac9
Jonathan Hartnett <jonathan.hartnett@eagles.oc.edu>
2017-04-18 12:15:09 -0500

    Added canceling animation to various other buttons around the website.

--- PPOK-Twilio/PPOK-Twilio/PageScripts/FillPrescription.js
+++ PPOK-Twilio/PPOK-Twilio/PageScripts/FillPrescription.js
@@ -1,47 +1,4 @@
 ﻿window.fillPrescription = (function ($) {
-    var warningID = null;
-    function protect(func, delay) {
-        //cancel the previous nav warning
-        if (warningID != null)
-            clearTimeout(warningID);
-
-        //enable navigation warning
-        window.onbeforeunload = function () { return true; };
-
-        warningID = setTimeout(function () {
-            window.onbeforeunload = null;
-        }, delay);
-
-        setTimeout(function () {
-            func();
-        }, delay);
-    }
-
-    function fill(button, id) {
-        if (confirm("Are you sure you want to fill this Prescription?")) {
-
-            var oldContent = button.innerHTML;
-            button.innerHTML = 'Cancel<div class="progress" />';
-            button.classList.remove('btn-primary');
-            button.classList.add('btn-danger');
-
-            protect(function () {
-                button.classList.remove('btn-danger');
-                button.classList.add('btn-primary');
-                button.innerHTML = oldContent;
-
-                $.ajax({
-                    type: "post",
-                    url: "/fillprescription/fill",
-                    data: { id },
-                    datatype: "json",
-                    success: function (r) {
-                        window.history.go(0);
-                    }
-                });
-            }, 5000);
-        }
-    };
     return {
         init: function () {
             console.log("Loading JS");
@@ -51,7 +8,6 @@
                 url: "/FillPrescription/GetAllFilledPrescriptions",
                 dataType: "json",
                 success: function (r) {
-                    console.log(r);
                     var dt = $('#example').DataTable({
                         "data": r,
                         "columns": [
@@ -62,18 +18,27 @@
                             {
                                 "data": "Code",
                                 "render": function (data, type, row) {
-                                    return "<button type=\"button\" class=\"btn btn-primary cancel-button\" onclick=\"window.fillPrescription.fill(this, " + data + ")\">Fill</button>";
+                                    return '<button type="button" class="btn btn-primary cancel-button" onclick="window.fillPrescription.click(' + data + ')" data-confirm="Are you sure you want to fill this Prescription?">Fill</button>';
                                 }
                             }
                         ]
 
                     });
+                    window.cancelButton.init();
                 }
             });
             console.log("finished loading js");
         },
-        fill: function (button, id) {
-            fill(button, id);
+        click: function (id) {
+            $.ajax({
+                type: "post",
+                url: "/fillprescription/fill",
+                data: { id },
+                datatype: "json",
+                success: function (r) {
+                    window.history.go(0);
+                }
+            });
         },
     }
 
